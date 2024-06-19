9ae598612d967615909c13bbb0cf5c15deae8cde
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2024-06-19 17:22:30 +0200

    Fix: seeder ProjectsTableSeeder

--- database/migrations/2024_06_19_145106_create_projects_table.php
+++ database/migrations/2024_06_19_145106_create_projects_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('portfolios', function (Blueprint $table) {
+        Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
@@ -22,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('portfolios');
+        Schema::dropIfExists('projects');
     }
 };
